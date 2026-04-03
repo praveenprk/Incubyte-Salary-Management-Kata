9770c817ee3c8b1cbf2509e0d5df9a4cd641ca83
praveenprk <praveenkumarkp666@gmail.com>
2026-04-03 15:31:54 +0530

    green: uncommented salary calc code to make the test pass

--- src/employee/salary.calculator.ts
+++ src/employee/salary.calculator.ts
@@ -1,12 +1,12 @@
-// export function calculateSalary(gross: number, country: string) {
-//   const c = country.toLowerCase()
-//   let tdsRate = 0
+export function calculateSalary(gross: number, country: string) {
+  const c = country.toLowerCase()
+  let tdsRate = 0
 
-//   if (c === 'india') tdsRate = 0.10
-//   else if (c === 'united states') tdsRate = 0.12
+  if (c === 'india') tdsRate = 0.10
+  else if (c === 'united states') tdsRate = 0.12
 
-//   const tds = gross * tdsRate
-//   const netSalary = gross - tds
+  const tds = gross * tdsRate
+  const netSalary = gross - tds
 
-//   return { tds, netSalary }
-// }
+  return { tds, netSalary }
+}
